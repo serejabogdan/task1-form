42826c4c14c163d6e7b0a91e3fcd0289f30c6824
Serhii Bohdan <serejabgdn@gmail.com>
2021-07-30 17:57:35 +0300

    created: saga middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from "./reducers/rootReducer";
+import createSagaMiddleware from 'redux-saga'
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, applyMiddleware(createSagaMiddleware()), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
