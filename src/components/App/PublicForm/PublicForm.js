aa987f49f041a3c61dc3d4cdb44071501c3ae6f8
Serhii Bohdan <serejabgdn@gmail.com>
2021-07-30 17:23:20 +0300

    cleared: PublicForm

--- src/components/App/PublicForm/PublicForm.js
+++ src/components/App/PublicForm/PublicForm.js
@@ -1,22 +1,16 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './PublicForm.css'
 
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {connect} from "react-redux";
 
-import { setToken } from '../../../redux/actions'
+import {setToken} from '../../../redux/actions'
 
-const PublicForm = ({setToken, token, ...props}) => {
-    console.log(props)
-
-    useEffect(() => {
-        console.log(token)
-    }, [token]);
-
+const PublicForm = (props) => {
 
     return (
         <div className="public-form">
@@ -24,14 +18,14 @@ const PublicForm = ({setToken, token, ...props}) => {
             <Form>
                 <FormGroup>
                     <Label for="username">Email</Label>
-                    <Input type="text" name="username" id="username" placeholder="Enter your name" />
+                    <Input type="text" name="username" id="username" placeholder="Enter your name"/>
                 </FormGroup>
                 <FormGroup>
                     <Label for="password">Password</Label>
-                    <Input type="password" name="password" id="password" placeholder="Enter password" />
+                    <Input type="password" name="password" id="password" placeholder="Enter password"/>
                 </FormGroup>
 
-                <Button onClick={() => setToken('123')}>Go!</Button>
+                <Button>Go!</Button>
             </Form>
         </div>
     );
@@ -43,7 +37,6 @@ PublicForm.propTypes = {
 }
 
 const mapStateToProps = (state) => {
-    console.log(state)
     return {
         token: state.token
     }
