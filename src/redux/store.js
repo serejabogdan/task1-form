d3a4fea62cf70b5af19f2e1b865f42f62673ee26
Serhii Bohdan <serejabgdn@gmail.com>
2021-07-30 18:00:14 +0300

    fixed: createStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,6 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from "./reducers/rootReducer";
 import createSagaMiddleware from 'redux-saga'
 
-const store = createStore(rootReducer, applyMiddleware(createSagaMiddleware()), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, applyMiddleware(createSagaMiddleware()));
 
 export default store;
