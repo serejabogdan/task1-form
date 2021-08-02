5cb55b5a2853378f424b85dee4f6f2378a2ba284
Serhii Bohdan <serejabgdn@gmail.com>
2021-08-02 10:42:17 +0300

    added: redux-form reducer; added: redux-form to PublicForm

--- src/components/App/PublicForm/PublicForm.js
+++ src/components/App/PublicForm/PublicForm.js
@@ -9,27 +9,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {connect} from "react-redux";
 
 import {setToken} from '../../../redux/actions'
+import {reduxForm, Field} from "redux-form";
 
 const PublicForm = (props) => {
+    function submit(values) {
+        console.log(values)
+    }
 
     return (
         <div className="public-form">
             <h1>Public form</h1>
-            <Form>
-                <FormGroup>
-                    <Label for="username">Email</Label>
-                    <Input type="text" name="username" id="username" placeholder="Enter your name"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="password">Password</Label>
-                    <Input type="password" name="password" id="password" placeholder="Enter password"/>
-                </FormGroup>
-
-                <Button>Go!</Button>
-            </Form>
+            <UserFormRedux onSubmit={submit}/>
         </div>
     );
 };
+
+function InputField ({input, type, id, label, meta: {touched, warning, error}, ...props}) {
+    const hasError = touched && ((error && <span>{error}</span>) || warning && <span>{warning}</span>);
+    return (
+        <div>
+            <Label for={id}>{label}</Label>
+            <Input id={id} placeholder={label} {...input}/>
+            {hasError}
+        </div>
+    );
+}
+
+const userForm = (props) => {
+    return (
+        <Form onSubmit={props.handleSubmit}>
+            <FormGroup>
+                <Field type="text" name="username" id="username" label="Username" component={InputField}/>
+            </FormGroup>
+            <FormGroup>
+                <Field type="password" name="password" id="password" label="Password" component={InputField}/>
+
+                {/*<Input type="password" name="password" id="password" placeholder="Enter password"/>*/}
+            </FormGroup>
+
+            <Button>Go!</Button>
+        </Form>
+    );
+}
+
+const UserFormRedux = reduxForm({form: 'userForm'})(userForm);
 
 PublicForm.propTypes = {
     token: PropTypes.string,
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
 import tokenReducer from "./tokenReducer";
 
+import {reducer as formReducer} from 'redux-form'
+
 export default combineReducers({
-    token: tokenReducer
+    token: tokenReducer,
+    form: formReducer
 });
